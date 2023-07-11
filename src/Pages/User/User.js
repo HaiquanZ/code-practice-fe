9232d163aa60d332e1ebca7d655dd9bb2ccad6fe
HaiquanZ <nn764562@gmail.com>
2023-07-11 14:36:01 +0700

    Prepare

---
+++ src/Pages/User/User.js
@@ -0,0 +1,15 @@
+import React from "react";
+import Header from "../../Layout/Header/Header";
+import Footer from "../../Layout/Footer/Footer";
+
+function User(){
+    return(
+            <div>
+                <Header/>
+                <h1>User</h1>
+                <Footer/>
+            </div>
+        )
+};
+
+export default User;
